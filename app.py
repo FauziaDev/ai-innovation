a0d11f6e39d7fb52780372734e6ef99aa3c2461a
FauziaDev <fauzianishat19@gmail.com>
2026-08-10 23:42:38 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,87 +1,10 @@
 import streamlit as st
-import os
-from dotenv import load_dotenv
-from google import genai
 
-load_dotenv()
+st.title("My First AI UI")
+st.write("This is a basic UI built with Streamlit")
 
-api_key = os.getenv("GEMINI_API_KEY")
+user_age = st.text_input("What is your age?")
+user_message = st.text_input("What is your favorite?")
 
-client = genai.Client(api_key=api_key)
-
-st.title("THE MULTIVERSE OF CHATBOATS")
-
-personality = st.sidebar.selectbox(
-    "Who do you want to talk to?",
-    [
-        "An Expert Hacker",
-        "Ironman",
-        "Any random personality",
-        "Virat Kohli",
-        "An angry Ravi Shastri",
-        "A crazy Ronaldo fan",
-        "Donald Trump"
-    ]
-)
-
-intensity = st.sidebar.slider(
-    "Intensity",
-    min_value=1,
-    max_value=10
-)
-
-user_message = st.text_input("Say Something:")
-
-# -----------------------------
-# TASK 5: Dynamic Avatars
-# -----------------------------
-if personality == "An Expert Hacker":
-    bot_avatar = "💻"
-elif personality == "Ironman":
-    bot_avatar = "🦾"
-elif personality == "Any random personality":
-    bot_avatar = "🎲"
-elif personality == "Virat Kohli":
-    bot_avatar = "🏏"
-elif personality == "An angry Ravi Shastri":
-    bot_avatar = "🎙️"
-elif personality == "A crazy Ronaldo fan":
-    bot_avatar = "⚽"
-elif personality == "Donald Trump":
-    bot_avatar = "🦅"
-else:
-    bot_avatar = "🤖"
-
-if st.button("SEND"):
-    if user_message:
-
-        ai_instructions = f"""
-        You are acting as {personality}
-        with an intensity level of {intensity}.
-
-        Respond to the user's message while staying completely
-        in character.
-
-        User message:
-        {user_message}
-        """
-
-        with st.spinner("Connecting to the multiverse!....."):
-
-            response = client.models.generate_content(
-                model="gemini-flash-latest",
-                contents=ai_instructions
-            )
-
-        # -----------------------------
-        # TASK 4: Chat UI elements
-        # -----------------------------
-        with st.chat_message("user"):
-            st.write(user_message)
-
-        with st.chat_message("assistant", avatar=bot_avatar):
-            st.write(response.text)
-
-    else:
-        st.warning("Please type a message first")
-        
+if st.button("Submit"):
+    st.write(user_message)
